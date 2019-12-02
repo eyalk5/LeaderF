7ebb18cc046ddd4b131cf785f5f8a10c81125f69
Eyal Karni <ekarni@preemptsecurity.com>
2019-12-02 21:47:25 +0200

    replace space

--- autoload/leaderf/python/leaderf/manager.py
+++ autoload/leaderf/python/leaderf/manager.py
@@ -2379,13 +2379,14 @@ class Manager(object):
                 self._index = 0 # search from beginning
                 if self._cli.pattern:
                     self._search(cur_content)
-            elif equal(cmd, '<C-K>'):
+                    #changes - eyal up <> down c-k c-j
+            elif equal(cmd, '<Up>'):
                 self._toUp()
                 self._previewResult(False)
-            elif equal(cmd, '<C-J>'):
+            elif equal(cmd, '<Down>'):
                 self._toDown()
                 self._previewResult(False)
-            elif equal(cmd, '<Up>'):
+            elif equal(cmd, '<C-K>'):
                 if self._cli.previousHistory(self._getExplorer().getStlCategory()):
                     if self._getInstance().isReverseOrder():
                         lfCmd("normal! G")
@@ -2393,7 +2394,7 @@ class Manager(object):
                         self._gotoFirstLine()
                     self._index = 0 # search from beginning
                     self._search(cur_content)
-            elif equal(cmd, '<Down>'):
+            elif equal(cmd, '<C-J>'):
                 if self._cli.nextHistory(self._getExplorer().getStlCategory()):
                     if self._getInstance().isReverseOrder():
                         lfCmd("normal! G")
